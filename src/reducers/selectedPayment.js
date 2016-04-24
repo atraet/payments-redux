efca55438ca41d69f4b35ce571cdb2684600a016
KZ <zhiping72@gmail.com>
2016-04-24 18:27:07 +0200

    When there is just one payment, set this as selected payment, and set period 1 as selected period

--- src/reducers/selectedPayment.js
+++ src/reducers/selectedPayment.js
@@ -1,9 +1,16 @@
-import {SELECT_PAYMENT} from '../actions/types';
+import {SELECT_PAYMENT, FETCH_PAYMENTS} from '../actions/types';
 
 export default function (state = null, action) {
     switch (action.type) {
         case SELECT_PAYMENT:
             return action.payload;
+        case FETCH_PAYMENTS:
+            let payments = action.payload.data;
+            // If there is just one payment,
+            // Select this payment
+            return payments.length === 1
+                ? payments[0].id
+                : state;
         default:
             return state;
     }
--- src/reducers/selectedPeriod.js
+++ src/reducers/selectedPeriod.js
@@ -1,9 +1,16 @@
-import {SELECT_PERIOD} from '../actions/types';
+import {SELECT_PERIOD, FETCH_PAYMENTS} from '../actions/types';
 
 export default function (state = null, action) {
     switch (action.type) {
         case SELECT_PERIOD:
             return action.payload;
+        case FETCH_PAYMENTS:
+            let payments = action.payload.data;
+            // If there is just one payment,
+            // Select period type 1
+            return payments.length === 1
+                ? 1
+                : state;
         default:
             return state;
     }
